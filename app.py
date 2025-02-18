3d5ebf1a50997ef97bf490489d06e22deb199fa6
Alberto96-star <albertorc1996@gmail.com>
2025-02-18 20:26:41 +0000

    implementando envio de correo

--- app.py
+++ app.py
@@ -1,18 +1,34 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request
+from decouple import config
+from send_email import send_email
 
-import os
 
 app = Flask(__name__)
 
+app.config.from_mapping(
+        SENDGRID_KEY=config('SENDGRID_KEY')
+    )
 
 @app.route('/')
 def index():
-    return render_template('inicio.html')
+    return render_template('partials/inicio.html')
+
 
 @app.route('/proyectos')
 def proyectos():
     return render_template('proyecto/proyectos.html')
 
+@app.route('/mail', methods=['GET', 'POST'])
+def mail():
+    name = request.form.get('name')
+    email = request.form.get('email')
+    message = request.form.get('message')
+
+    if request.method == 'POST':
+        send_email(name, email, message)
+        return render_template('partials/sent_mail.html')
+    
+    return render_template('partials/inicio.html') 
 
 # la configuracion de debug debe estar al final
 if __name__ == "__main__":

0fead97c877a8c7287ea1da53f4d392bf9da1177
Alberto96-star <albertorc1996@gmail.com>
2025-02-19 10:06:48 +0000

    agregando cambios envio mail

--- app.py
+++ app.py
@@ -1,15 +1,26 @@
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, jsonify
 from decouple import config
-from send_email import send_email
+from sendgrid.helpers.mail import *
+from sendgrid import SendGridAPIClient
+import sendgrid
+import os
 
 
 app = Flask(__name__)
 
+# Configuración de las variables de entorno de Vercel
+SENDGRID_KEY = os.environ.get('SENDGRID_KEY')
+FROM_EMAIL = os.environ.get('FROM_EMAIL')
+
+# Verifica si las variables de entorno están configuradas
+if not SENDGRID_KEY or not FROM_EMAIL:
+    raise ValueError("Las variables SENDGRID_KEY y FROM_EMAIL deben estar configuradas en Vercel.")
+
 app.config.from_mapping(
-        SENDGRID_KEY=config('SENDGRID_KEY')
+        SENDGRID_KEY= 'SENDGRID_KEY'
     )
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
     return render_template('partials/inicio.html')
 
@@ -20,16 +31,34 @@ def proyectos():
 
 @app.route('/mail', methods=['GET', 'POST'])
 def mail():
-    name = request.form.get('name')
-    email = request.form.get('email')
-    message = request.form.get('message')
-
     if request.method == 'POST':
+        name = request.form.get('name')
+        email = request.form.get('email')
+        message = request.form.get('message')
         send_email(name, email, message)
         return render_template('partials/sent_mail.html')
-    
     return render_template('partials/inicio.html') 
 
+
+def send_email(name, email, message):
+    sg = sendgrid.SendGridAPIClient(api_key=SENDGRID_KEY)
+    from_email = Email(FROM_EMAIL)
+    to_email = To(FROM_EMAIL, substitutions={
+        "-name-": name,
+        "-email-": email,
+        "-message-": message,
+    })
+
+    html_content = """
+        <p>Hola Alberto, tienes un nuevo contacto desde la web:</p>
+        <p>Nombre: -name-</p>
+        <p>Correo: -email-</p>
+        <p>Mensaje: -message-</p>
+    """
+    email = Mail(from_email, to_email, 'Nuevo contacto desde la web', html_content=html_content)
+    response = sg.send(email)
+
+    
 # la configuracion de debug debe estar al final
 if __name__ == "__main__":
     app.run(debug=True)
